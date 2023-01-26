700d74aca8142399c19cdd03487638d3eff96e05
Lailiyanurfadila <109324295+Lailiyanurfadila@users.noreply.github.com>
2023-01-26 15:29:12 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,6 @@
+<?php
+$conn=mysqli_connect("localhost","root","","lailiya_26jan");
+?>
+
+
+
---
+++ tambah.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+    <table border = "1">
+    <h1>Input Data Barang </h1>
+    <form action="proses-tambah.php" method="POST">
+        <form>
+            <fieldset>
+<p>
+    <label for="nama_barang">Nama Barang:</label>
+    <input type="text" name="nama_barang"/>
+</p>
+<p>
+    <label for="kategori_barang">Kategori Barang :</label>
+    <input type="text" name="kategori_barang"/>
+</p>
+<p>
+    <label for="nama_karyawan">Nama Karyawan :</label>
+    <input type="text" name="nama_karyawan"/>
+</p>
+<p>
+    <label for="divisi">Divisi :</label>
+    <input type="text" name="divisi" />
+</p>
+<p>
+    <label for="jabatan">Jabatan :</label>
+    <input type="text" name="jabatan" />
+</p>
+<p>
+    <input type="submit" value="kirimdata" name="kirimdata" />
+</p>
+</fieldset>
+</table>
+</body>
+</html>
+
